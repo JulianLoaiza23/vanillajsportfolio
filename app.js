bf5ea03a636d75b275e65802481b6f3bb9513aa9
JulianLoaiza23 <mail.loaiza@gmail.com>
2024-09-22 18:20:08 -0400

    Add prettier rc and eslint separate server and app files

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
+const app = express();
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+//MIDDLEWARE
+app.use(express.static(`${__dirname}/public`));
+
+mongoose
+  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("Connection error", error));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/submit-form", async (req, res) => {
+  const searchItem = await req.body.searchForThis;
+
+  console.log(searchItem);
+
+  res.send(`We can search for this item on our backend:  ${searchItem}`);
+
+  ///extract form data
+  ///do something with form data
+  ///load a page that says Hey you successfully sent a message, click here to go back home
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,46 +1,6 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-
-dotenv.config({ path: "./config.env" });
-
-const app = express();
+const app = require('./app');
 const port = process.env.PORT || 3000;
-const DB = process.env.DATABASE.replace(
-  "<PASSWORD>",
-  process.env.DATABASE_PASSWORD
-);
-
-//MIDDLEWARE
-app.use(express.static(`${__dirname}/public`));
-
-mongoose
-  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((error) => console.error("Connection error", error));
-
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
-app.post("/submit-form", async (req, res) => {
-  const searchItem = await req.body.searchForThis;
-
-  console.log(searchItem);
-
-  res.send(`We can search for this item on our backend:  ${searchItem}`);
-
-  ///extract form data
-  ///do something with form data
-  ///load a page that says Hey you successfully sent a message, click here to go back home
+    console.log(`Example app listening on port ${port}`);
 });
