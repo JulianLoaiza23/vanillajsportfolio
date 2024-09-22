d7188faff7589ffb94c7136689ee2c22abaf1f03
JulianLoaiza23 <mail.loaiza@gmail.com>
2024-09-22 17:35:44 -0400

    restructure folders and add express

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+//MIDDLEWARE
+app.use(express.static(`${__dirname}/public`));
+
+mongoose
+  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("Connection error", error));
 
 app.use(
   express.urlencoded({
@@ -12,41 +29,18 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/styles.scss", (req, res) => {
-  res.sendFile(__dirname + "/styles.scss");
-});
-
-app.get("/script.js", (req, res) => {
-  res.sendFile(__dirname + "/script.js");
-});
-
-app.get("/_colors.scss", (req, res) => {
-  res.sendFile(__dirname + "/_colors.scss");
-});
-
-app.get("/_stars.scss", (req, res) => {
-  res.sendFile(__dirname + "/_stars.scss");
-});
-
-app.get("/assets/head_shot.jpg", (req, res) => {
-  res.sendFile(__dirname + "/assets/head_shot.jpg");
-});
-
-app.get("/assets/nebula.jpg", (req, res) => {
-  res.sendFile(__dirname + "/assets/nebula.jpg");
-});
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
 app.post("/submit-form", async (req, res) => {
-  const searchItem = await req.body.searchForThis
-  console.log(searchItem)
+  const searchItem = await req.body.searchForThis;
 
-  res.send(`We can search for this item on our backend:  ${searchItem}`)
+  console.log(searchItem);
 
+  res.send(`We can search for this item on our backend:  ${searchItem}`);
 
-  //extract form data
-  //
-})
+  ///extract form data
+  ///do something with form data
+  ///load a page that says Hey you successfully sent a message, click here to go back home
+});
