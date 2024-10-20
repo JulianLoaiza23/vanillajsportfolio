da513a39575a3574662ed22d95f2cbccce01fa76
JulianLoaiza23 <mail.loaiza@gmail.com>
2024-10-20 19:18:42 -0400

    refactored code

--- public/projects/pokedex/pokedex.js
+++ public/projects/pokedex/pokedex.js
@@ -1,14 +1,14 @@
 (function () {
   const pokemonSearchContainer = document.querySelector('#pokemonSearch');
   const searchButton = document.querySelector('#searchButton');
-
-  searchButton.addEventListener('click', myFunction);
+  
+  searchButton.addEventListener('click', searchPokemon);
 
   searchButton.disabled = true;
 
   pokemonSearchContainer.addEventListener('keyup', (keypressEvt) => {
     if (keypressEvt.code === 'Enter') {
-      myFunction();
+      searchPokemon();
     }
 
     const userInput = keypressEvt.target;
@@ -24,10 +24,12 @@
 
   //define variables for h1 element here ONE FOR THE INOPUT ONE FOR STRING VALUE OF INPUT ONE FOR THE LENGTH
 
-  function myFunction() {
+  function searchPokemon() {
     const spriteResult = document.querySelector('#pokeSprite');
+    const statResults = document.querySelectorAll('#pokemonStatsCtn > div');
+    const errorMsgCtn = document.querySelector('#errorCtn');
+
     const pokemonName = pokemonSearchContainer.value;
-    const errorMessage = document.querySelector('#pokeHeader');
 
     fetch('https://pokeapi.co/api/v2/pokemon/' + pokemonName) // Replace with your API URL
       .then((response) => {
@@ -37,15 +39,68 @@
         return response.json(); // Assuming the API returns JSON data
       })
       .then((data) => {
-        spriteResult.src = data.sprites.front_default; //dot notation
-        errorMessage.innerText = '';
+        setPokemon(
+          data,
+          spriteResult,
+          errorMsgCtn,
+          statResults
+        );
       })
       .catch((error) => {
-        // Handle any errors that occurred during the fetch innertext
-
-        //set h1 'innerText' here
-        errorMessage.innerText = 'Sorry This Pokemon has yet to be Discovered.';
+        errorMsgCtn.classList.remove('hide');
+        errorMsgCtn.textContent = 'Sorry This Pokemon has yet to be Discovered.';
         spriteResult.src = '';
+
+        for (let ctn of statResults) {
+          ctn.textContent = '';
+        }
+
+        console.error(error);
       });
+  }
+
+  function setPokemon(
+    data,
+    spriteResult,
+    errorMsgCtn,
+    statResults
+  ) {
+    const pokemon = {
+      image: data.sprites.front_default,
+      name: data.name,
+      id: data.id,
+      abilities: [],
+      types: [],
+    };
+
+    for (let ability of data.abilities) {
+      pokemon.abilities.push(ability.ability.name);
+    }
+
+    for (let type of data.types) {
+      pokemon.types.push(type.type.name);
+    }
+
+    for (let i = 0; i < statResults.length; i++) {
+      switch (i) {
+        case 0:
+          statResults[i].textContent = 'Name: ' + pokemon.name;
+          break;
+        case 1:
+          statResults[i].textContent = 'ID: ' + pokemon.id;
+          break;
+        case 2:
+          statResults[i].textContent = 'Abilities: ' + pokemon.abilities.join(', ');
+          break;
+        case 3:
+          statResults[i].textContent = 'Type: ' + pokemon.types.join(', ');
+          break;
+      }
+    }
+
+    spriteResult.src = pokemon.image; //dot notation
+    
+    errorMsgCtn.classList.add('hide');
+    errorMsgCtn.textContent = '';
   }
 })();
